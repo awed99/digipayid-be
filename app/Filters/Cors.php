2ca6f334aa6ac16a61cc2e7eb8ea02779eb10b06
Dewa Danu Brata <dewadanubrata@gmail.com>
2024-07-19 19:32:25 +0700

    fix error new v4

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -27,7 +27,7 @@ class Cors implements FilterInterface
     {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Requested-Method, Authorization, X-Signature, X-Timestamp");
+        // header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Requested-Method, Authorization, X-Signature, X-Timestamp");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
         if ($method == "OPTIONS") {
